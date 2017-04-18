6aca7f229143715ea60a0d770f3e3face2410290
aaron <aarongt_@hotmail.com>
2017-04-18 17:03:33 +0100

    Added files
    Added:
    Index.html
    Main.js
    Main.css

---
+++ main.js
@@ -0,0 +1,17 @@
+var numGen = Math.floor(Math.random() * 1000 + 1);
+var attempts = 0;
+
+function guessingButton() {
+  attempts++;
+  var usersGuess = document.getElementById('numInput').value;
+
+  if (usersGuess == "") {
+    alert("Please enter a make a guess in the input");
+  } else if (usersGuess == numGen) {
+    alert("You guessed the right number! " + attempts + " attempts.");
+  } else if (usersGuess < numGen) {
+    alert("You guessed too small!");
+  } else {
+    alert("You guessed too big!");
+  }
+}
